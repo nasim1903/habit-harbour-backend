f86f91c38f62def48cafb80c4b84afd0b22ff405
azotamiota <azotamiota@gmail.com>
2022-08-16 22:45:34 +0100

    divide authorised and unauthorised routes

--- api/api.js
+++ api/api.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const api = express()
 const cors = require('cors')
-const habitsController = require('./controllers/habits')
+const authorisedRoute = require('./routes/authorised')
 const userController = require('./controllers/user')
 require('dotenv').config()
 
@@ -9,17 +9,13 @@ api.use(cors())
 api.use(express.json())
 api.use(express.urlencoded({extended: false}))
 
-// All GET requests
-api.get('/', (req, res) => {res.json({message: 'Welcome to Habit Harbour'})})
-api.get('/dashboard', userController.findAll) //  verifyToken to add as middleware
-api.get('/dashboard/:username', userController.findUser) //  verifyToken to add as middleware
-api.get('/dashboard/:username/habits', habitsController.showHabits)
+// Authorised routes
+api.use('/dashboard', authorisedRoute)
 
-//All POST requests
+// Unauthorised routes
+api.get('/', (req, res) => {res.json({message: 'Welcome to Habit Harbour'})})
 api.post('/register', userController.createUser)
 api.post('/login', userController.login)
-api.post('/dashboard/:username/habits', habitsController.updateHabits)
-api.post('/dashboard/:username/habits/increment-streak', habitsController.incrementStreak)
 
 
 module.exports = api;
---
+++ api/routes/authorised.js
@@ -0,0 +1,16 @@
+const router = require('express').Router()
+const userController = require('../controllers/user')
+const habitsController = require('../controllers/habits')
+const verifyToken = require('../middleware/verifyToken')
+
+// router.use(verifyToken) // make this as a comment for developing purposes 
+
+router.get('/', userController.findAll) 
+router.get('/:username', userController.findUser) 
+router.get('/dashboard/:username/habits', habitsController.showHabits) 
+
+router.post('/:username/habits', habitsController.updateHabits)
+router.post('/:username/habits/increment-streak', habitsController.incrementStreak)
+
+
+module.exports = router
