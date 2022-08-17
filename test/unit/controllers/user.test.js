a5ef5c00ae7cf92c552eeb40b8d01e078b105634
Your Name <you@example.com>
2022-08-17 14:46:31 +0100

    Finally fixed create a new user test

--- test/unit/controllers/user.test.js
+++ test/unit/controllers/user.test.js
@@ -1,15 +1,22 @@
 const supertest = require('supertest')
 const app = require('../../../api/api')
-const User = require('../../../api/models/User')
+const mongoose = require('mongoose');
 
 describe('User controllers', () => {
     let api;
 
-    beforeAll(() => {
-        api = app.listen(5050, () => {
-            console.log('Test server is listening on port 5050');
+    beforeAll(async () => {
+        await mongoose
+        .connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+            useUnifiedTopology: true,
         })
-    })
+        .then(() => {
+            api = app.listen(5050)
+        })
+      });
 
     afterAll((done) => {
         console.log('Gracefully stopping the server');
@@ -23,29 +30,37 @@ describe('User controllers', () => {
     })
 
 
-    it('it checks a sample credential', (done) => {
-        supertest(api)
-            .get('/login')
-            .auth('admin6', 'password6')
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /text/)
-            .end(done)
-        // .expect(200, done);
-    });
-    // it('it creates a new user', async () => {
-    //     const response = await supertest(api).post('/register')
-    //         .send({
-    //         "username": "admin98",
-    //         "password" : "password99"
-    //         })
+    // it('it checks a sample credential', (done) => {
+    //     supertest(api)
+    //         .get('/login')
+    //         .auth('admin6', 'password6')
     //         .set('Accept', 'application/json')
-    //         .expect('Content-Type', /json/)
-    //         .expect(200)
-    //         .end((err, res) => {
-    //             if (err) return done(err);
-    //             return done();
-    //         })
-    // })
-})
+    //         .expect('Content-Type', /text/)
+    //         .end(done)
+    //     .expect(200, done);
+    // });
+
+    it('it creates a new user', async () => {
+        const response = await supertest(api).post('/register')
+        .send({
+            "username": "admin98",
+            "password" : "password99"
+        })
+        .set('Accept', 'application/json');
+        
+            console.log(response.headers)
+            expect(response.headers["content-type"]).toMatch(/json/);
+            expect(response.body.username).toBeDefined();
+
+            expect(response.body.message).toBeDefined();
+
+            expect(response.statusCode).toEqual(201)
+            // .end((err, res) => {
+            //     if (err) return done(err);
+            //     return done();
+            // Example test that uses async
+            })
+    })
+
 
 
